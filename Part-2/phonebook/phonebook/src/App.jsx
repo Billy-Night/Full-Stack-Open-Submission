26d7fbd329407a991b412608d2986d263df69b42
Billy <billy.m.nightingale@gmail.com>
2026-09-04 12:44:14 +0200

    Exercise 2.11: The Phonebook Step 6 Completed

--- Part-2/phonebook/phonebook/src/App.jsx
+++ Part-2/phonebook/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ filter, onFilterChange }) => {
   return (
@@ -18,7 +19,6 @@ const PersonForm = ({
 }) => {
   return (
     <div>
-      <h3>Add a new ££</h3>
       <form onSubmit={onSubmit}>
         <div>
           name: <input value={name} onChange={onNameChange} />
@@ -49,15 +49,18 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("Effect started");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("Promise Fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
